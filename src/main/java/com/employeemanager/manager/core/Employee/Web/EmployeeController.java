032b16830b76a4162d7b0fa947394089a0409d3b
Prithvi <prithvisinghtewatia@gmail.com>
2022-03-12 21:57:00 +0530

    Created Employee Controller

---
+++ src/main/java/com/employeemanager/manager/core/Employee/Web/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.employeemanager.manager.core.Employee.Web;
+
+import com.employeemanager.manager.core.Employee.Employee;
+import com.employeemanager.manager.core.Employee.EmployeeService;
+import com.employeemanager.manager.error.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService=employeeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployee(){
+        List<Employee> employeeList=employeeService.findAllEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) throws UserNotFoundException {
+        Employee employee=employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee,HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
